81013dae0b94cb336439418b39a95a56264080c8
aliaalsadadi <126590042+aliaalsadadi@users.noreply.github.com>
2023-08-10 21:56:57 +0300

    Update qrcode_reader.py

--- qrcode_reader.py
+++ qrcode_reader.py
@@ -1,3 +1,4 @@
 from ultralytics import YOLO
+#change path of model to best.pt path
 model = YOLO(r"C:\Users\HP\PycharmProjects\pythonProject1\yolo\runs\detect\train2\weights\best.pt")
 results = model(r'C:\Users\HP\PycharmProjects\pythonProject1\smart_fridge\test.jpg',save=True,conf=0.7,imgsz=256)[0]
