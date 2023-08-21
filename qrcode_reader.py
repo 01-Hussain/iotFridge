94fed5c3f2e4f38d652766d39e5964d4b59013db
aliaalsadadi <126590042+aliaalsadadi@users.noreply.github.com>
2023-08-21 11:10:47 +0300

    Update qrcode_reader.py

--- qrcode_reader.py
+++ qrcode_reader.py
@@ -1,11 +1,11 @@
 from ultralytics import YOLO
 import glob
 import os
-model_directory = r"C:\Users\HP\Downloads\iotFridge-main\iotFridge-main\runs\detect"
+# model_directory = r"C:\Users\HP\Downloads\iotFridge-main\iotFridge-main\runs\detect"
 
-model_files = glob.glob(f"{model_directory}/train*/weights/best.pt")
-latest_model_file = max(model_files, key=os.path.getctime)
-model = YOLO(latest_model_file)
+# model_files = glob.glob(f"{model_directory}/train*/weights/best.pt")
+# latest_model_file = max(model_files, key=os.path.getctime)
+model = YOLO('best.pt')
 
 results = model('try.jpg',conf=0.5,imgsz=256)[0]
 for detection in results.boxes.data.tolist():
