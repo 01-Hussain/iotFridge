648438b7e7740b136a7ca2339f0b191b4bb4f721
aliaalsadadi <126590042+aliaalsadadi@users.noreply.github.com>
2023-08-25 10:32:12 +0300

    Update model.js

--- model.js
+++ model.js
@@ -1,30 +1,68 @@
 const axios = require('axios');
 const fs = require('fs');
-function get_images() {
-    function get_image(image_name, folder) {
-        let url = 'http://192.168.100.200/capture';
-        axios.get(url, { responseType: 'arraybuffer' })
-            .then(response => {
-                fs.writeFileSync(`images/${folder}/${image_name}.jpg`, Buffer.from(response.data, 'binary'));
-            });
-    }
 
-    function processImages(folder) {
-        let i = 1;
-        const totalIterations = 10;
+function getHighestNumberedFile(directoryPath) {
+    try {
+        const files = fs.readdirSync(directoryPath); // Read the directory synchronously
 
-        function processNextImage() {
-            if (i <= totalIterations) {
-                get_image(`${i}`, folder);
-                i++;
+        if (files.length === 0) {
+            return 0; // No files in the directory
+        }
 
-                setTimeout(processNextImage, 500);
+        let highestNumber = -Infinity;
+
+        for (const file of files) {
+            const numberMatch = file.match(/(\d+)/); // Extract numbers from the file name
+            if (numberMatch) {
+                const number = parseInt(numberMatch[0], 10);
+                if (number > highestNumber) {
+                    highestNumber = number;
+                }
             }
         }
 
-        processNextImage();
+        return highestNumber;
+    } catch (error) {
+        console.error('Error reading directory:', error);
+        return -Infinity; // Error reading directory
+    }
+}
+
+async function get_image(image_name, folder) {
+    try {
+        let url = 'http://192.168.100.200/capture';
+        const response = await axios.get(url, { responseType: 'arraybuffer' });
+        fs.writeFileSync(`images/${folder}/${image_name}.jpg`, Buffer.from(response.data, 'binary'));
+    } catch (error) {
+        console.error(`Error getting image ${image_name}:`, error);
+    }
+}
+
+async function processImages(folder, highestNumber) {
+    let i = highestNumber + 1;
+
+    if (i === 0) {
+        i = 1; // No files in the directory, start from 1
     }
 
+    const totalIterations = i + 10; // Take another 10 pictures from the next number onwards
+
+    for (let j = i; j <= totalIterations; j++) {
+        await get_image(j.toString(), folder);
+        await new Promise(resolve => setTimeout(resolve, 100));
+    }
+}
+
+async function get_images() {
+    const highesttrain = getHighestNumberedFile("images/train/");
+    const highestval = getHighestNumberedFile("images/val/");
+
+    await processImages('train', highesttrain);
+    await new Promise(resolve => setTimeout(resolve, 3000));
+    await processImages('val', highestval);
+}
+
+exports.get_images = get_images;
 
     processImages('train');
 
